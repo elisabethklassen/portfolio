3f4e7cf4caa1a2b4d984cde9fbd2c019efb42835
Elisabeth Klassen <elisabethklassen@uvic.ca>
2021-01-16 23:30:40 +0000

    adding push functions and add number function

--- 2048/2048.h
+++ 2048/2048.h
@@ -6,4 +6,9 @@ typedef struct board_wrap {
 } board_wrap;
 
 void create_board(board_wrap* board, int size);
+void add_random(board_wrap* board);
+void push_up(board_wrap* board);
+void push_down(board_wrap* board);
+void push_left(board_wrap* board);
+void push_right(board_wrap* board);
 void print_board(board_wrap* board);
